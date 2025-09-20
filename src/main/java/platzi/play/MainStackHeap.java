a6808a1f9a84ec7c012707a4767c8f9476d2d294
Pacool84 <flopez_1384@hotmail.com>
2025-09-20 10:00:10 -0600

    [ADD] Gestión de memoria en Java: stack, heap y garbage collector

---
+++ src/main/java/platzi/play/MainStackHeap.java
@@ -0,0 +1,17 @@
+package platzi.play;
+
+import platzi.play.contenido.Pelicula;
+
+public class MainStackHeap {
+    public static void main(String[] args) {
+        Pelicula reyLeon = new Pelicula("El rey Leon", 120, "Animación");
+        Pelicula harryPotter = new Pelicula("Harry Potter", 150, "Fantasía");
+
+        reyLeon = harryPotter; // Ambas variables apuntan al mismo objeto en memoria heap
+
+        reyLeon.titulo = "El Hobbit"; // Modificando el título a través de la referencia reyLeon, cualquier valor que se modifique a través de una referencia afectará al mismo objeto en memoria heap
+
+        System.out.println(reyLeon.titulo);
+        System.out.println(harryPotter.titulo);
+    }
+}
